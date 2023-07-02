55cfac1221e41d328d4da2c732a49690ef5064c8
timur <timurka.zavyalov@mail.ru>
2023-07-02 15:28:18 +0300

    fix: changed IP-address and routes

--- src/queries.ts
+++ src/queries.ts
@@ -4,15 +4,15 @@ import { serverUrl} from "./env";
 export const getHistory = (data) => {
     console.log(data)
     //"id": "Виноградник №2"
-    // return axios.post(serverUrl + '/predict', data)
-    return axios.get(serverUrl + '/map')
+    return axios.post(serverUrl + '/map', data)
+//     return axios.get(serverUrl + '/map')
 }
 export const getLastHistoryItem = (data) => {
-    // return axios.post(serverUrl + '/last_data',data)
-    return axios.get(serverUrl + '/last_date')
+    return axios.post(serverUrl + '/last_date',data)
+//     return axios.get(serverUrl + '/last_date')
 }
 export const predict = (data) => {
-    // return axios.post('/predict', data)
-    return axios.get(serverUrl+ '/predict')
+    return axios.post(serverUrl + '/predict', data)
+//     return axios.get(serverUrl+ '/predict')
 
 }
